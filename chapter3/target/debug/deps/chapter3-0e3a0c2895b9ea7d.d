6a4c8d78723d018ec247b6dd2d2958e01d4a7b66
Heeseung Jang <jang5934@gmail.com>
2022-01-10 00:03:19 +0900

    Test code directory structure has been rearranged.

---
+++ chapter3/target/debug/deps/chapter3-0e3a0c2895b9ea7d.d
@@ -0,0 +1,5 @@
+c:\work_space\rust_projects\rust_test\chapter3\target\debug\deps\chapter3-0e3a0c2895b9ea7d.rmeta: src\main.rs
+
+c:\work_space\rust_projects\rust_test\chapter3\target\debug\deps\chapter3-0e3a0c2895b9ea7d.d: src\main.rs
+
+src\main.rs:
---
+++ chapter4/target/debug/deps/chapter4-bea6959f319eb21b.d
@@ -0,0 +1,5 @@
+c:\work_space\rust_projects\rust_test\chapter4\target\debug\deps\chapter4-bea6959f319eb21b.rmeta: src\main.rs
+
+c:\work_space\rust_projects\rust_test\chapter4\target\debug\deps\chapter4-bea6959f319eb21b.d: src\main.rs
+
+src\main.rs:
